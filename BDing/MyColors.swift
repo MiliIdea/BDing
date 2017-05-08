050c8a61188668ee0997c52567e77ea947017952
Milad <milikarimi@yahoo.com>
2017-05-08 05:19:49 -0700

    set colors

---
+++ BDing/MyColors.swift
@@ -0,0 +1,31 @@
+//
+//  MyColors.swift
+//  BDing
+//
+//  Created by MILAD on 5/7/17.
+//  Copyright © 2017 MILAD. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyColors{
+    
+    static var color1 : UIColor = UIColor(hex: "eceff1")
+    
+    static var color2 : UIColor = UIColor(hex: "37474f")
+    
+    static var color3 : UIColor = UIColor(hex: "455a64")
+    
+    static var color4 : UIColor = UIColor(hex: "2196f3")
+    
+    static var color5 : UIColor = UIColor(hex: "78909c")
+    
+    static var color6 : UIColor = UIColor(hex: "d50000")
+    
+    static var color7 : UIColor = UIColor(hex: "00c853")
+
+    
+    
+    
+}
